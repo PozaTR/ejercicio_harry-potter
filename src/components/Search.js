cc53ff58b08a0920cf10ac420853f5d0e2172786
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-14 13:23:15 +0200

    add some basic styles in search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import '../styles/search.scss';
 
 class Search extends React.Component {
     render() {
       const { findCharacter, searchCharacter, houseOptions, genderOptions } = this.props;
         return(
             <div className="search__container" >
+              <div>
                 <label htmlFor="name">Busca tu personaje favorito</label>
-                <input id="name" type="text" name="name"  onChange={searchCharacter} value={findCharacter} />
+                <input className="search__input" id="name" type="text" name="name"  onChange={searchCharacter} value={findCharacter} />
+              </div>
                 <div className="select__container__house" >
                   <label htmlFor="house" >Casa</label>
-                  <select id="house" onChange={searchCharacter}>
+                  <select className="search__input" id="house" onChange={searchCharacter}>
                     <option value="" >Todas</option>
                     {houseOptions.map((houseOption, index) => 
                       <option key={index} value={houseOption} >{houseOption}</option>
@@ -18,7 +21,7 @@ class Search extends React.Component {
                 </div>
                 <div className="select__container__gender" >
                   <label htmlFor="gender" >Género</label>
-                  <select id="gender" onChange={searchCharacter}>
+                  <select className="search__input" id="gender" onChange={searchCharacter}>
                     <option value="" >Ambos</option>
                     {genderOptions.map((genderOption, index) =>
                       <option key={index} value={genderOption} >{genderOption}</option>
