47d1507bb241d0e7b9dac6cdfc4622dc5013ef90
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-15 12:12:50 +0200

    create docs

---
+++ docs/precache-manifest.7b8ce4bb5a9389b3e7b1f70a19dc3bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9fd7fbc3eac45e6629d7a842c80886",
+    "url": "./index.html"
+  },
+  {
+    "revision": "736e82def7cad0fd289d",
+    "url": "./static/css/main.25724d03.chunk.css"
+  },
+  {
+    "revision": "2a8461d5583be432bd6f",
+    "url": "./static/js/2.515f6d28.chunk.js"
+  },
+  {
+    "revision": "736e82def7cad0fd289d",
+    "url": "./static/js/main.0afeeea0.chunk.js"
+  },
+  {
+    "revision": "70ae50e230f9dbeaf23e",
+    "url": "./static/js/runtime-main.3472ae2d.js"
+  },
+  {
+    "revision": "1c70474734329c930eea0f071a33e311",
+    "url": "./static/media/logo-harry-potter.1c704747.png"
+  }
+]);
