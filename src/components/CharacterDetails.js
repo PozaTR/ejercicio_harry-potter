9fe398e256eceaa134a1884380c2c1a2d3bd6dc3
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-15 11:29:27 +0200

    add link styles to back characterList

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -17,24 +17,24 @@ const  CharacterDetails = props =>  {
       const {name, species, image, house, dateOfBirth, ancestry, actor}= character[0];
 
       return(
-        <React.Fragment>
-          <div className="details" >
-            <div>
-              <h2 className="details__name">{name}</h2>
-              <p>{actor}</p>
-            </div>
-            <div className="details__image-container" >
-                <img className="details__image" src={image} alt={name}></img>
-            </div>
-            <div>
-              <p>{house}</p>
-              <p>{species}</p>
-              <p>{dateOfBirth}</p>
-              <p>{ancestry}</p>
+          <div className="details">
+             <Link className="details__link" to="/" >Vuelve al listado</Link>
+            <div className="detail" >
+              <div>
+                <h2 className="detail__name">{name}</h2>
+                <p>{actor}</p>
+              </div>
+              <div className="detail__image-container" >
+                  <img className="detail__image" src={image} alt={name}></img>
+              </div>
+              <div>
+                <p className="detail__information">{house}</p>
+                <p className="detail__information">{species}</p>
+                <p className="detail__information">{dateOfBirth}</p>
+                <p className="detail__information">{ancestry}</p>
+              </div>
             </div>
           </div>
-          <Link to="/" >Vuelve al listado</Link>
-        </React.Fragment>
       )
     } else {
       return(
